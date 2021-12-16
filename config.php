4b169348b5cc951f47ea37822618df2c5fd045de
thisnotdefault <thisnotdefault@gmail.com>
2021-12-16 15:14:42 +0300

    add solution for lesson6

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const SERVER = 'localhost';
-const LOGIN = "root";
-const PASS = "test!";
-const DB = "gallery";
-
-$connect = mysqli_connect(SERVER,LOGIN,PASS,DB) or die("Ошибка подключения к базе данных.");
